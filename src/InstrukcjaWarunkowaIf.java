afc9b2b65a4700a53e87c7fd7d43cb66072c7bd4
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-06-30 09:23:53 +0200

    Commit Instrukcja warunkowa

---
+++ src/InstrukcjaWarunkowaIf.java
@@ -0,0 +1,11 @@
+public class InstrukcjaWarunkowaIf {
+    public static void main(String[] args) {
+        float dzielna = 4.0F;
+        float dzielnik = 2.0F;
+
+        if(dzielnik != 0) {
+            System.out.println("Wynik dzielenia to: " + dzielna / dzielnik);
+        }
+        System.out.println("Podaj inną liczbę: ");
+    }
+}
