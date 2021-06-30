ca623d0792d95c1f3f079f102124883ed3dfdf2e
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-06-30 09:54:11 +0200

    Commit zadanie ageChecker

---
+++ src/AgeChecker.java
@@ -0,0 +1,19 @@
+import com.sun.security.jgss.GSSUtil;
+
+import java.util.Scanner;
+
+public class AgeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj swój wiek: ");
+        int age = scanner.nextInt();
+
+        if(age < 18 && age >=0 ) {
+            System.out.println("Nie można ci sprzedać alkoholu");
+        }else if(age < 0) {
+            System.out.println("Wprowadź poprawną wartość");
+        }else {
+            System.out.println("Dziękuje za zakupy w moim sklepie");
+        }
+    }
+}
