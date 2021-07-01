94f5d71b63f9d55b8b729f07bb0a50dcf7086dd1
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-07-01 08:33:29 +0200

    Commit DoWhileLoop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int number = 21;
+        do {
+            System.out.println("Moja wartość to: " + number + "I jestem mniejsza od 20");
+        }while (number < 20);
+    }
+}
