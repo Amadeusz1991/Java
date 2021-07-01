5379b5e21380221871a0869a2d7ad37b7c0d219f
Amadeusz1991 <amadeusz.jankowski@capgemini.com>
2021-07-01 08:19:43 +0200

    Commit ForLoop

---
+++ src/PętlaFor.java
@@ -0,0 +1,17 @@
+public class PętlaFor {
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        for (int i = 0; i < number; i = i + 2){
+            System.out.print(i);
+        }
+        for (int i = 0; i < 100; i++){
+            if(i % 5 == 0);
+            System.out.println(i);
+        }
+        for (int sth = 0; sth < 300; sth++){
+            System.out.println("Będę robił prace domową");
+        }
+    }
+}
